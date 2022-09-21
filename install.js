3cbd2e43ae054526f1be841cce006702c725357a
jd <jean.daniel.michaud@gmail.com>
2022-09-21 21:51:48 +0200

    Fix windows links

--- install.js
+++ install.js
@@ -2,6 +2,30 @@
 
 const fs = require('fs');
 
+function createLinkWin(from, to) {
+  fs.writeFileSync(to,
+`#!/usr/bin/env sh
+${from}.exe "$@"
+`);
+
+  fs.writeFileSync(`${to}.cmd`,
+`@ECHO off
+GOTO start
+:find_dp0
+SET dp0=%~dp0
+EXIT /b
+:start
+SETLOCAL
+CALL :find_dp0
+
+"${from.replaceAll('/', '\\')}.exe" %*
+`);
+}
+
+function createLinkLinux(from, to) {
+  fs.symlink(from, to, 'file', (err) => { if (err !== undefined) console.error(err); });
+}
+
 const arch = `${process.arch}-${process.platform}`;
 const distPath = `dist/${arch}`;
 if (!fs.existsSync(distPath)) {
@@ -11,23 +35,33 @@ if (!fs.existsSync(distPath)) {
   process.exit(1);
 }
 
-fs.mkdirSync('../../.bin/');
-fs.symlink(`${__dirname}/${distPath}/clang-15`, '../../.bin/clang', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/lld`, '../../.bin/lld', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/lld`, '../../.bin/wasm-ld', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/lld`, '../../.bin/ld64.lld', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/lld`, '../../.bin/ld.lld', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/lld`, '../../.bin/lld-link', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/spectest-interp`, '../../.bin/spectest-interp', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm2c`, '../../.bin/wasm2c', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm2wat`, '../../.bin/wasm2wat', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-decompile`, '../../.bin/wasm-decompile', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-interp`, '../../.bin/wasm-interp', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-objdump`, '../../.bin/wasm-objdump', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-opcodecnt`, '../../.bin/wasm-opcodecnt', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-strip`, '../../.bin/wasm-strip', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wasm-validate`, '../../.bin/wasm-validate', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wast2json`, '../../.bin/wast2json', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wat2wasm`, '../../.bin/wat2wasm', 'file', (err) => { if (err !== undefined) console.error(err); });
-fs.symlink(`${__dirname}/${distPath}/wat-desugar`, '../../.bin/wat-desugar', 'file', (err) => { if (err !== undefined) console.error(err); });
+try {
+  fs.mkdirSync('../../.bin/');
+} catch (e) {
+  if (e.code !== 'EEXIST') {
+    throw e;
+  }
+}
 
+const createLink = process.platform === 'linux' ? createLinkLinux : createLinkWin;
+const cwd = __dirname.replaceAll('\\', '/');
+
+if (process.platform === 'linux') {
+  createLink(`${cwd}/${distPath}/clang-15`, '../../.bin/clang');
+  createLink(`${cwd}/${distPath}/lld`, '../../.bin/wasm-ld');
+  } else {
+  createLink(`${cwd}/${distPath}/clang`, '../../.bin/clang');
+  createLink(`${cwd}/${distPath}/wasm-ld`, '../../.bin/wasm-ld');
+}
+createLink(`${cwd}/${distPath}/spectest-interp`, '../../.bin/spectest-interp');
+createLink(`${cwd}/${distPath}/wasm2c`, '../../.bin/wasm2c');
+createLink(`${cwd}/${distPath}/wasm2wat`, '../../.bin/wasm2wat');
+createLink(`${cwd}/${distPath}/wasm-decompile`, '../../.bin/wasm-decompile');
+createLink(`${cwd}/${distPath}/wasm-interp`, '../../.bin/wasm-interp');
+createLink(`${cwd}/${distPath}/wasm-objdump`, '../../.bin/wasm-objdump');
+createLink(`${cwd}/${distPath}/wasm-opcodecnt`, '../../.bin/wasm-opcodecnt');
+createLink(`${cwd}/${distPath}/wasm-strip`, '../../.bin/wasm-strip');
+createLink(`${cwd}/${distPath}/wasm-validate`, '../../.bin/wasm-validate');
+createLink(`${cwd}/${distPath}/wast2json`, '../../.bin/wast2json');
+createLink(`${cwd}/${distPath}/wat2wasm`, '../../.bin/wat2wasm');
+createLink(`${cwd}/${distPath}/wat-desugar`, '../../.bin/wat-desugar');
